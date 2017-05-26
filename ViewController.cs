0f172241766ebc2f3a1355f94156772a02da2a95
munsra <piero.silvestri93@gmail.com>
2017-05-26 15:10:22 +0200

    App End

---
+++ ViewController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Threading.Tasks;
+using UIKit;
+using Xamarin.Media;
+
+namespace XamarinIOSCamera
+{
+	public partial class ViewController : UIViewController
+	{
+		MediaPicker mediaPicker;
+		private TaskScheduler uiScheduler;
+		MediaPickerController mediaPickerController;
+		UIAlertController alertView;
+
+		protected ViewController(IntPtr handle) : base(handle)
+		{
+			// Note: this .ctor should not contain any initialization logic.
+		}
+
+		public override void ViewDidLoad()
+		{
+			base.ViewDidLoad();
+			// Perform any additional setup after loading the view, typically from a nib.
+			TitleLabel.Text = "Simple take photo";
+
+			mediaPicker = new MediaPicker();
+
+			TakePhotoButton.TouchUpInside += TakePhoto;
+		}
+
+		public override void DidReceiveMemoryWarning()
+		{
+			base.DidReceiveMemoryWarning();
+			// Release any cached data, images, etc that aren't in use.
+		}
+
+		private void TakePhoto(object sender, EventArgs e)
+		{
+			if (!mediaPicker.IsCameraAvailable)
+			{
+				alertView = UIAlertController.Create ("Camera Error", "Camera is not available on this device.", UIAlertControllerStyle.Alert);
+				alertView.AddAction (UIAlertAction.Create ("Ok", UIAlertActionStyle.Default, null));
+
+				// Present Alert
+				PresentViewController(alertView, true, null);
+
+				return;
+			}
+
+			mediaPickerController = mediaPicker.GetTakePhotoUI(new StoreCameraMediaOptions
+			{
+				Name = "Photo1.jpg",
+				Directory = "MyTeoDirectory"
+			}
+															  );
+
+			PresentViewController(mediaPickerController, true, null);
+
+			mediaPickerController.GetResultAsync().ContinueWith(t =>
+			{
+				ImageBoxView.Image = UIImage.FromFile(t.Result.Path);
+				DismissViewController(true, null);
+			}, uiScheduler);
+		}
+	}
+}
---
+++ ViewController.designer.cs
@@ -0,0 +1,46 @@
+// WARNING
+//
+// This file has been generated automatically by Xamarin Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
+//
+using Foundation;
+using System;
+using System.CodeDom.Compiler;
+
+namespace XamarinIOSCamera
+{
+    [Register ("ViewController")]
+    partial class ViewController
+    {
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIImageView ImageBoxView { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton TakePhotoButton { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UILabel TitleLabel { get; set; }
+
+        void ReleaseDesignerOutlets ()
+        {
+            if (ImageBoxView != null) {
+                ImageBoxView.Dispose ();
+                ImageBoxView = null;
+            }
+
+            if (TakePhotoButton != null) {
+                TakePhotoButton.Dispose ();
+                TakePhotoButton = null;
+            }
+
+            if (TitleLabel != null) {
+                TitleLabel.Dispose ();
+                TitleLabel = null;
+            }
+        }
+    }
+}
